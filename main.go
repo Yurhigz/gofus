fdc0efc1f88cb683af7eb10d243310224aa7b1db
Benoit.G <benoitgaugiran@gmail.com>
2025-07-13 15:13:23 +0200

    Coorection import path

--- main.go
+++ main.go
@@ -1,37 +1,20 @@
 package main
 
 import (
-<<<<<<< HEAD
-	"encoding/hex"
-	"fmt"
+	"gofus/config"
+	"gofus/internal/proxy"
 	"log"
 )
 
 func main() {
-	hexData := "261a240a220a13747970652e616e6b616d612e636f6d2f687a72120b0a0908a3c11c10a30418012a1a280a260a13747970652e616e6b616d612e636f6d2f687a71120f0a0d08a3c11c10082205080a108e12"
-	data, err := hex.DecodeString(hexData)
+	cfg, err := config.Load()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Erreur de configuration: %v", err)
 	}
 
-	fmt.Println("Données décodées:", data)
-	fmt.Printf("Données en string : %s\n", string(data))
-=======
-    "log"
-    
-    "gofus/internal/proxy"
-    "gofus/config"
-)
+	proxy := proxy.New(cfg.ClientAddr, cfg.ServerAddr)
+	if err := proxy.Start(); err != nil {
+		log.Fatalf("Erreur du proxy: %v", err)
+	}
 
-func main() {
-    cfg, err := config.Load()
-    if err != nil {
-        log.Fatalf("Erreur de configuration: %v", err)
-    }
-
-    proxy := proxy.New(cfg.ClientAddr, cfg.ServerAddr)
-    if err := proxy.Start(); err != nil {
-        log.Fatalf("Erreur du proxy: %v", err)
-    }
->>>>>>> e8a42577e055c88370a3bcd640a2e49875f63825
 }
